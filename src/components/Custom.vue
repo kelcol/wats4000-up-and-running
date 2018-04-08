f5851b0b05b59291629ea240df49a51fa56ac64c
Kelly Colht <colhk@seattleu.edu>
2018-04-08 11:34:39 -0700

    Added directives for custom heading

--- src/components/Custom.vue
+++ src/components/Custom.vue
@@ -1,29 +1,18 @@
-<template>
+<template style>
   <div class="custom">    
       <input type="text" v-model="custom" placeholder="What's your name?" >
-      <!-- <button v-on:click="personalizeHeading(you)" v-on:keyup="pressKey">Submit</button> -->
       <br />
-      <h1>{{custom}} Markdown Editor</h1>
+      <h1 v-if="custom.length === 0">Markdown Editor</h1>
+      <h1 v-else>{{custom}}'s Markdown Editor</h1>
   </div>
 </template>
 
-
 <script>
-export default {
-  name: 'custom',    
+export default {  
   data() {
     return {
       custom: "",
     }
   },
-  methods: {
-    personalizeHeading: function(custom){
-      console.log(custom);      
-  },
-  // This doesn't work
-  pressKey: function(custom) {
-    return custom + "'s";
-      }
-}
 }
 </script>
